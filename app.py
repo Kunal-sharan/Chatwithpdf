22c07654ce24e592727cfdd5748a960f2ca2f7ce
Kunal sharan <codedragonks07@gmail.com>
2024-03-15 02:03:48 +0530

    u

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if key:
             
             user_query=st.text_input("Ask Question about your pdf:")
             if user_query:
-                user_query+=" explain using the pdf"
+                user_query+=" explain using the pdf and include relevant quotations taken directly from pdf in double quotes"
                 docs=knowledge_base.similarity_search(user_query)
                 # st.write(docs)
                 llm=OpenAI(openai_api_key=f"{key}")
