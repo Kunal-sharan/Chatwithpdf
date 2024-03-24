fcebbaf4d9619783c42c85e81cb18c4669d901ee
Kunal sharan <codedragonks07@gmail.com>
2024-03-25 02:39:30 +0530

    u

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import OpenAI
+# from langchain.memory import ConversationBufferMemory
+# from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
 from htmlTemplates import css, bot_template, user_template
 # from langchain.llms import HuggingFaceHub
 from PIL import Image
@@ -42,33 +44,6 @@ def get_vectorstore(text_chunks,key):
     return vectorstore
 
 
-def get_conversation_chain(vectorstore,key):
-    llm = ChatOpenAI(openai_api_key=f"{key}")
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
-
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-
-
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs",
                         page_icon=":books:")
@@ -77,26 +52,23 @@ def main():
     key=st.text_input("Enter your Open AI  API Key")
     
     if key:
-
-        if "conversation" not in st.session_state:
-            st.session_state.conversation = None
-        if "chat_history" not in st.session_state:
-            st.session_state.chat_history = None
-
         st.header("Chat with multiple PDFs :books:")
-        user_question = st.text_input("Ask a question about your documents:")
-        b=st.button("Submit")
-        if user_question:
-            if b:
-                handle_userinput(user_question)
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        raw_text = get_pdf_text(pdf_docs)
 
-        with st.sidebar:
-            st.subheader("Your documents")
-            pdf_docs = st.file_uploader(
-                "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-            if st.button("Process"):
-                with st.spinner("Processing"):
-                    # get pdf text
+        # get the text chunks
+        text_chunks = get_text_chunks(raw_text)
+
+        # create vector store
+        vectorstore = get_vectorstore(text_chunks,key) 
+        st.success("Done")   
+        with st.spinner("Processing"):
+                # get pdf text
+                with st.sidebar:
+                    st.header("Image Extracted from Docs: ")
+
                     for pdf in pdf_docs:
                         pdf_read=Pdf.open(pdf)
                         for i in  range(len(pdf_read.pages)):
@@ -110,18 +82,25 @@ def main():
                                 # embedding = ibed.to_embeddings(pdf_image.as_pil_image())
                                 # ar=list(embedding)
                                 # st.write(ar)
-                    
-                    raw_text = get_pdf_text(pdf_docs)
+                
+                
+                
+        user_question = st.text_input("Ask a question about your documents:")
+        b=st.button("Submit")
+        if user_question:
+            if b:
+                user_ques=f"{user_question} explain and include relevant quotations taken directly from pdfs in double quotes"
+                docs=vectorstore.similarity_search(user_ques)
+                # st.write(docs)
+                llm=OpenAI(openai_api_key=f"{key}")
+                chain=load_qa_chain(llm,chain_type="stuff")
+                with get_openai_callback() as cb:
+                    response=chain.run(input_documents=docs,question=user_ques)
+                    st.write(response)
+                    st.write(cb)
 
-                    # get the text chunks
-                    text_chunks = get_text_chunks(raw_text)
-
-                    # create vector store
-                    vectorstore = get_vectorstore(text_chunks,key)
-                    st.success("Done")
-                    # create conversation chain
-                    st.session_state.conversation = get_conversation_chain(
-                        vectorstore,key)
+            
+                       
             
 
 
