94af039302401baa8427534bc31ade7c1f312d41
Kunal sharan <codedragonks07@gmail.com>
2024-04-20 20:19:57 +0530

    u

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ import pikepdf
 from imgbeddings import imgbeddings
 from langchain_anthropic import AnthropicLLM
 from googleapiclient.discovery import build
+from fpdf import FPDF
+import base64
+from io import BytesIO
+from youtubesearchpython import VideosSearch
 # from langchain_google_genai import GoogleGenerativeAI
 
 # You need to replace YOUR_API_KEY with your actual API Key
@@ -48,7 +52,9 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-
+def create_download_link(val, filename):
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
 def chat_with_claud(question, vectorstore, claud_key):
     if question:
@@ -63,6 +69,8 @@ tut_ques=[]
 imp=[]
 arr_list=[]
 imp_list=[]
+ques_ans=[]
+ans=[]
 template = """use this and only this {ass_chunk} chunk of text to extract the complete questions present in it
 question:
 """
@@ -135,13 +143,17 @@ if key and claud_key and youtube_key :
                         # st.write(response)
                         tut_ques.append(response)
                         st.write(tut_ques)
-                newarr=[]              
+                newarr=[]
+                
+                lin_yt=[]
+                # youtube = build('youtube', 'v3', developerKey=f'{youtube_key}')              
                 with st.spinner("Processing..."):
                     if 'vectorstore' in st.session_state:
                         for i in range(0,len(tut_ques)):
                             vectorstore=st.session_state.vectorstore
                             arr=tut_ques[i].split("\n")
-                            st.write(arr)
+                            # st.write(arr)
+    
                             for i in range(0,len(arr)):
                                 if len(arr[i])>0:
                                     newarr.append(arr[i])
@@ -152,6 +164,7 @@ if key and claud_key and youtube_key :
                                     # llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=f'{api_key}')
 
                                     chain=load_qa_chain(llm,chain_type="stuff")
+                                    # chain = load_qa_chain(AnthropicLLM(model='claude-2.1', anthropic_api_key=claud_key), chain_type="stuff")
                                     with get_openai_callback() as cb:
                                         try:
                                             response=chain.run(input_documents=tut_topics,question=arr[i])
@@ -160,48 +173,146 @@ if key and claud_key and youtube_key :
                                             response="No data"
                                             res_list=""
                                         st.write(response)
+                                        ans.append(response)
                                         imp.append(res_list)
                                         st.write(cb)
-                        # st.write(imp)
-                        for i in range(0,len(imp)):
-                            arr_list=imp[i].split("\n")
-                            for j in range(0,len(arr_list)):
-                                imp_list.append(arr_list[j])   
-                        # st.write(imp_list)
-                        youtube = build('youtube', 'v3', developerKey=f'{youtube_key}')
-                        yt_data=[]
-                        for i in range(0,len(imp_list)):
-                            if len(imp_list[i])>0:
+                        
+                                    arr_list=res_list.split("\n")
+                                    # for j in range(0,len(arr_list)):
+                                    #     imp_list.append(arr_list[j])   
+                                # st.write(imp_list)
                                 
-                                
-                                search_response = youtube.search().list(
-                                            q=f'{imp_list[i]}',
-                                            part='id,snippet',
-                                            maxResults=1
-                                        ).execute()
-                                for search_result in search_response.get('items', []):
-                                    if search_result['id']['kind'] == 'youtube#video':
-                                        video_data = {
-                                                "topic":f"{imp_list[i]}",
-                                                "title": f"{search_result['snippet']['title']}",
-                                                "links": f"https://www.youtube.com/watch?v={search_result['id']['videoId']}",
-                                                "desc":f"Description: {search_result['snippet']['description']}"
+                                    # st.write(arr_list)
+                                    yt_data=[]
+                                    for i in range(0,len(arr_list)):
+                                        if len(arr_list[i])>0:
+                                            
+                                            
+                                            # search_response = youtube.search().list(
+                                            #             q=f'{arr_list[i]}',
+                                            #             part='id,snippet',
+                                            #             maxResults=1
+                                            #         ).execute()
+                                            # for search_result in search_response.get('items', []):
+                                            #     if search_result['id']['kind'] == 'youtube#video':
+                                            #         video_data = {
+                                            #                 "topic":f"{arr_list[i]}",
+                                            #                 "title": f"{search_result['snippet']['title']}",
+                                            #                 "links": f"https://www.youtube.com/watch?v={search_result['id']['videoId']}",
+                                            #                 "desc":f"Description: {search_result['snippet']['description']}"
+                                            #             }
+                                            videoSearch=VideosSearch(f"{arr_list[i]}",limit=1)
+                                            video_data={
+                                                "title":f"{str(videoSearch.result()['result'][0]['title'])}",
+                                                "links":f"{videoSearch.result()['result'][0]['link']}",
+                                                "thumbnail":f"{videoSearch.result()['result'][0]['thumbnails'][0]['url']}"
                                             }
-                                        yt_data.append(video_data)
-                        num_rows = len(yt_data) // 1
-                        if len(yt_data) % 1 != 0:
-                            num_rows += 1
+                                            yt_data.append(video_data)
+                                    st.write(yt_data)        
+                                    lin_yt.append(yt_data)                    
+                            ques_ans.append(newarr)            
+                            ques_ans.append(ans)
+                            ques_ans.append(imp)
+                            ques_ans.append(lin_yt)
+                            st.session_state.data=ques_ans
+                            
+        if "data" in st.session_state:
+            # Create an "Export Report" button
+            export_as_pdf = st.button("Export Report")
+            q=st.session_state.data
+            
+# If the button is clicked
+            if export_as_pdf:
+                # st.write(q)
+                # st.write(len(ques_ans))
+                pdf = FPDF()
+                pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
+                pdf.add_font('DejaVuB', '', 'DejaVuSans-Bold.ttf', uni=True)
+                pdf.add_font('DejaVuN', '', 'DejaVuSans.ttf', uni=True)
 
-                        for i in range(num_rows):
-                            cols = st.columns(1)
-                            for j in range(1):
-                                index = i * 1 + j
-                                if index < len(yt_data):
-                                    tile = cols[j].container(height=700)
-                                    tile.write(yt_data[index]["topic"])
-                                    tile.write(yt_data[index]["title"])
-                                    tile.video(yt_data[index]["links"])
-                                    tile.write(yt_data[index]["desc"])
+                for i in range(0,len(st.session_state.data[0])):
+                    pdf.set_margins(10, 10, 5)
+                    pdf.add_page()
+                    report_text=""
+                    links=""
+                        #  links+=f"""<h1>Title:</h1><h2>{n_t}</h2>
+                        #             <br>
+                        #             <A HREF="{lnk}">{lnk}</A>
+                        #             """ 
+
+                    report_text = f"Q: {str(st.session_state.data[0][i])} \n\n {str(st.session_state.data[1][i])}\n\n Important topics based on the anwsers: {str(st.session_state.data[2][i])}\n\n All Relevant Video Links: \n {links} "
+#                     report_html="""<h1>Question</h1>
+# <p>{str(st.session_state.data[0][i])}</p>
+
+# <h1>Answer</h1>
+# <p>{str(st.session_state.data[1][i])}</p>
+
+# <h1>Important Topics</h1>
+# <ul>
+#     <li> {str(st.session_state.data[2][i])}</li>
+    
+# </ul>
+
+# <h1>Video Links</h1>
+# <ul>
+#     <li><a href="[1](https://www.youtube.com/watch?v=dQw4w9WgXcQ)">Video 1</a></li>
+#     <li><a href="[2](https://www.youtube.com/watch?v=3tmd-ClpJxA)">Video 2</a></li>
+#     <li><a href="[3](https://www.youtube.com/watch?v=ktvTqknDobU)">Video 3</a></li>
+# </ul>
+# """
+                    pdf.set_font('DejaVu', '', 14)
+                    # pdf.multi_cell(0,10, report_text)
+                    # pdf.set_font("Arial", size = 12)
+                    
+                    # Section 1
+                    pdf.multi_cell(0, 10, txt = "Q: " + str(st.session_state.data[0][i]), ln = True)
+                    pdf.set_font('DejaVuB', '', 12)
+
+                    # Section 2
+                    pdf.multi_cell(0, 10, txt ="A: "+ str(st.session_state.data[1][i])+"\n", ln = True)
+                    pdf.set_font('DejaVuB', '', 14)
+                    pdf.set_text_color(255, 0, 0)
+                    # Section 3
+                    pdf.multi_cell(0, 10, txt = "Important topics based on the answers: " , ln=True)
+                    pdf.set_text_color(0, 0, 0)
+                    pdf.set_font('DejaVu', '', 12)
+                    important_topics = str(st.session_state.data[2][i])
+                    pdf.multi_cell(0, 10, txt = important_topics,ln=True)
+                    pdf.set_font('DejaVu', '', 14)
+                    pdf.multi_cell(0,10,txt="\n"+"IMPORTANT TOPICS AND LINKS: ",ln=True)
+                    for j in range(0,len(st.session_state.data[3][i])):
+                        title=st.session_state.data[3][i][j]["title"]
+                        t_arr=title.split(" ")
+                        n_t=' '.join(map(str, t_arr))
+                        lnk=st.session_state.data[3][i][j]["links"]
+                        pdf.set_font('DejaVuB', '', 12)
+                        pdf.multi_cell(0, 10, txt = f"{n_t}",ln=True )
+                        pdf.set_font('DejaVu', '', 12)
+                        pdf.set_text_color(0, 0, 255)
+                        pdf.multi_cell(0, 10, txt = f"{lnk}" , ln=True)
+                        pdf.set_text_color(0, 0, 0)
+
+
+                         
+                html = create_download_link(pdf.output(dest="S"), "test")
+
+                st.markdown(html, unsafe_allow_html=True)
+
+                        
+                        # num_rows = len(yt_data) // 1
+                        # if len(yt_data) % 1 != 0:
+                        #     num_rows += 1
+
+                        # for i in range(num_rows):
+                        #     cols = st.columns(1)
+                        #     for j in range(1):
+                        #         index = i * 1 + j
+                        #         if index < len(yt_data):
+                        #             tile = cols[j].container(height=700)
+                        #             tile.write(yt_data[index]["topic"])
+                        #             tile.write(yt_data[index]["title"])
+                        #             tile.video(yt_data[index]["links"])
+                        #             tile.write(yt_data[index]["desc"])
                                 
 
                         
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import streamlit as  st 
-import time
+import fitz  # this is pymupdf
+pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'")
+# Open the document
+if pdf_docs :
+    
+    doc = fitz.open(stream=pdf_docs.read())  
 
-@st.cache_data
-def long_running_function():
-    time.sleep(5)  # This could be a long running computation
-    return "Expensive Result"
+    # Select the page you want to extract text from
+    page = doc.load_page(4)  
 
-result = long_running_function()
-st.write(result)
+    # Get the text of the page
+    text = page.get_text()
+    st.write(text)
+    text = text.encode('ascii', 'ignore')
+    # Now 'text' contains the text of the page in Unicode
+    st.write(str(text))
